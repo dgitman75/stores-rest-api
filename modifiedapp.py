1a60b92ac0728ce7c6496ef6ac05c0f2da293982
root <dmitry.gitman@macys.com>
2018-07-13 16:33:05 -0400

    Fixed not importing db when running the app

--- modifiedapp.py
+++ modifiedapp.py
@@ -12,10 +12,6 @@ app.secret_key = 'Dmitry'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-	db.create_all()
-
 jwt = JWT(app, authenticate, identity) # Created new end point /auth
 
 api.add_resource(Store, '/store/<string:name>')
